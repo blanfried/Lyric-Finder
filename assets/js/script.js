b84d417308d19a37da324d709e54a029edcb7201
Baer Lanfried <blanfried@yahoo.com>
2022-08-09 20:35:55 +0100

    adding js functionality to search bar (appending artist and song value to the base api url

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,29 @@ const artistInput = document.querySelector('#artist-input');
 const songInput = document.querySelector('#song-input');
 const artist = artistInput.value;
 const song = songInput.value;
+const searchBtn = document.querySelector('#search-btn');
 
-
-function appendURL() {
-      
+function createUrl() {
+      let baseUrl = 'https://api.lyrics.ovh/v1/'
+      function appendArtist() {
+            if (onclick.searchBtn) {
+                  baseUrl.append(artist + "/")
+            }
+      }
+      function appendSong() {
+            if (onclick.searchBtn) {
+                  baseUrl.append(song)
+            }
+      }
+      appendArtist();
+      appendSong();
 }
 
+console.log(createUrl());
 
-var requestUrl = 'https://www.stands4.com/services/v2/lyrics.php';
+
+
+var requestUrl = 'https://api.lyrics.ovh/v1/{artist}/{song}';
 
   fetch(requestUrl)
     .then(function (response) {
