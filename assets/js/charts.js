1c95111823febe2b809118f8f54483c0b07ea3c2
EthanD'Mello <dmello.ethan.98@gmail.com>
2022-08-16 00:27:30 +0100

    html and JS for appending data from API

--- assets/js/charts.js
+++ assets/js/charts.js
@@ -1,17 +1,50 @@
+const songChart = document.querySelector("#songChart");
+const artistChart = document.querySelector("#artistChart");
+
+// fetch top ten songs and append to HTML
 fetch(
-  "https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=1e24541a1d58fbe7992421c03d6c8516&format=json"
+  "https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=1e24541a1d58fbe7992421c03d6c8516&format=json&limit=10"
 )
   .then((response) => response.json())
   .then((data) => {
     console.log("Success:", data);
+    appendSongs(data);
   })
   .catch((error) => {
     console.error("Error:", error);
   });
 
-const songsChart = document.getElementById("songs-chart");
-const artistChart = document.getElementById("artist-chart");
+function appendSongs(data) {
+  let trackAr = data.tracks.track;
+  let trackList = [];
+  trackAr.forEach((tracks, i) => {
+    trackList.push(trackAr[i].name);
+  });
+  trackList.forEach((track, i) => {
+    songChart.children[i].textContent = track;
+  });
+}
 
-function show(data) {
-  let tab = <tr></tr>;
+// fetch and append top ten artist to HTML
+fetch(
+  "https://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=1e24541a1d58fbe7992421c03d6c8516&format=json&limit=10"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("Success:", data);
+    appendArtist(data);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+
+function appendArtist(data) {
+  let artistAr = data.artists.artist;
+  let artistList = [];
+  artistAr.forEach((tracks, i) => {
+    artistList.push(artistAr[i].name);
+  });
+  artistList.forEach((artist, i) => {
+    artistChart.children[i].textContent = artist;
+  });
 }
