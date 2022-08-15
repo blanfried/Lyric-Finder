0800154552e7f11dd99cefdc0cf3bad1810b9dea
nafisa-am <nafisa.am91@gmail.com>
2022-08-15 21:37:58 +0100

    adding show data feature

--- assets/js/charts.js
+++ assets/js/charts.js
@@ -1,23 +1,17 @@
-var top10 = document.querySelector ('#top10');
+fetch(
+  "https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=1e24541a1d58fbe7992421c03d6c8516&format=json"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("Success:", data);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
 
-const songInput = document.querySelector('#song-input');
-var request = new XMLHttpRequest();
-request.open(
-  "GET",
-  "https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=1e24541a1d58fbe7992421c03d6c8516&format=json",
-  true
-);
+const songsChart = document.getElementById("songs-chart");
+const artistChart = document.getElementById("artist-chart");
 
-request.onload = function () {};
-request.send();
-function getData() {
-    const response = await fetch('https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=1e24541a1d58fbe7992421c03d6c8516&format=json')
-    const data = await response.json()
-  }
-
-  var data = JSON.parse(this.response)
-
-data.forEach(song => {
- 
-  console.log(song.title)
-})
+function show(data) {
+  let tab = <tr></tr>;
+}
