6d320ae4acc31ba5d9c6eb4917a46d6aff8b6544
Nafisa Mohamed <nafisa@Nafisas-MacBook-Air.local>
2022-08-15 19:09:46 +0100

    making changes

--- assets/js/charts.js
+++ assets/js/charts.js
@@ -1,28 +1,22 @@
-var notification = document.querySelector ('#top10');
+const album = document.getElementById("#top-10");
 
-const notification = document.querySelector('#top-10');
-var request = new XMLHttpRequest();
-request.open(
-  "GET",
-  "https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=1e24541a1d58fbe7992421c03d6c8516",
-  true
-);
+fetch(
+  "https://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=disco&api_key=6c7aa94be66ddd84cad6986fca45fe45&format=json"
+)
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then((data) => {
+    console.log(data);
+    displayAlbum(data);
+  })
+  .catch((error) => console.error("FETCH ERROR:", error));
 
-request.onload = function () {};
-request.send();
-function getData() {
-    const response = await fetch('https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=1e24541a1d58fbe7992421c03d6c8516')
-    const data = await response.json()
-  }
-
-  var data = JSON.parse(this.response)
-
-data.forEach(song => {
- 
-  console.log(song.title)
-})
-
-function display_songInput(data) {
-  const lyricsBox = document.querySelector("#top10");
-  document.getElementById("top-10").value = data["top=10"];
+function displayAlbum(data) {
+  const album = data.album[0];
+  const albumDiv = document.getElementById("album");
 }
